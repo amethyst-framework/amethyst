8e98d78497a3e3e6cef39880dc40d3342540771e
codcore <codcore@gmail.com>
2015-06-19 21:59:45 +0300

    Fix RoutesPainter

--- src/amethyst/support/routes_painter.cr
+++ src/amethyst/support/routes_painter.cr
@@ -6,7 +6,7 @@ module RoutesPainter
   # The synopsis of arguments receive is Rails-like:
   # get "/products/:id", "products#show"
   # where 'products' is a controller named ProductsController, and "show" is it's action
-  def get_controller_and_action(string : String)
+  def get_controller_and_action(pattern : String, controller_action : String)
     pattern.gsub(/\$/, "\$") unless pattern == "/"
     controller, action = controller_action.split("#")
     controller = controller.capitalize+"Controller"
@@ -14,31 +14,31 @@ module RoutesPainter
   end
 
   def get(pattern, controller_action)
-    get_controller_and_action(pattern)
+    route = get_controller_and_action(pattern, controller_action)
     route.add_request_method("GET")
     @routes << route
   end
 
   def put(pattern, controller_action)
-    get_controller_and_action(pattern)
+    route = get_controller_and_action(pattern, controller_action)
     route.add_request_method("PUT")
     @routes << route
   end
 
   def post(pattern, controller_action)
-    get_controller_and_action(pattern)
+    route = get_controller_and_action(pattern, controller_action)
     route.add_request_method("POST")
     @routes << route
   end
 
   def delete(pattern, controller_action)
-    get_controller_and_action(pattern)
+    route = get_controller_and_action(pattern, controller_action)
     route.add_request_method("DELETE")
     @routes << route
   end
 
   def all(pattern, controller_action)
-    get_controller_and_action(pattern)
+    route = get_controller_and_action(pattern, controller_action)
     route.add_request_method("GET")
     route.add_request_method("POST")
     route.add_request_method("PUT")
