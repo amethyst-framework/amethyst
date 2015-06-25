0ed7be16058a0ea73389fb8c5d37e3aed3bba938
codcore <codcore@gmail.com>
2015-06-25 17:11:28 +0300

    Fix cookie building

--- src/amethyst/http/response.cr
+++ src/amethyst/http/response.cr
@@ -22,9 +22,9 @@ class Response
   end
 
   def cookie(key, value, secure=false, http_only=false, path="", domain="", expires="")
-    cookie_string = "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
     cookie_string = String.build do |string|
-      string <<  "; domain=#{domain}" unless domain.to_s.empty?
+      string << "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
+      string << "; domain=#{domain}" unless domain.to_s.empty?
       string << "; path=#{path}" unless path.to_s.empty?
       string << "; secure"  if secure
       string << "; HttpOnly" if http_only
