df638b3fa411b4cd962257b8dd7d72d1b708541d
codcore <codcore@gmail.com>
2015-06-27 11:22:51 +0300

    Improve Sendable to call methods with arguments

--- src/amethyst/support/sendable.cr
+++ src/amethyst/support/sendable.cr
@@ -1,21 +1,51 @@
 module Sendable
+  
+  class WrongNumberOfArguments < Exception
+    def initialize(method, given, expected)
+      super("Wrong number of arguments for '#{method}' (#{given} for #{expected})")
+    end
+  end
+  
+  class WrongInstanceMethod < Exception
+    def initialize(klass, method)
+      super("Instance of '#{klass}' class has no '#{method}' method")
+    end
+  end
 
   # Allows to invoke objects methods through 'send' method.
-  # Only methods without args can be invoked
   macro create_send
     case method
       {% methods = @type.methods %}
       {% for m in methods %}
-      {% if m.args.empty? %}
+      {% args = m.args %}
       when "{{m.name.id}}"
-        {{m.name.id}}
+        raise WrongNumberOfArguments.new({{m.name.stringify}}, args.length, {{m.args.length}}) unless args.length == {{m.args.length}}
+        {{m.name.id}}{% unless args.empty? %}(
+        {% for arg in args %}{{arg.id}}=args[:{{arg.id}}],{% end %}){% end %}
         {% end %}
-        {% end %}
-      else raise "Can't call #{method} through send"
+      else raise WrongInstanceMethod.new({{@type.name.stringify}}, "#{method}")
       end
+
   end
 
-  def send(method)
+def send(method, args={} of Symbol => String)
     create_send
   end
 end
+
+# class A
+#   include Sendable
+
+#   def call(a,b)
+#     p a
+#     p b
+#   end
+
+#   def hello
+#    p "hello"
+#   end
+# end
+
+# a = A.new
+# a.send "call", {a: "a", b: "b"}
+# a.send "hello"
