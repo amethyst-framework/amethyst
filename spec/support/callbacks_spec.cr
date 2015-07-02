621b3cfc745212cf71618d7286e753ccc08c1372
codcore <codcore@gmail.com>
2015-07-02 21:25:13 +0300

    Fix and add specs for CallbackSequence

---
+++ spec/support/callbacks_spec.cr
@@ -0,0 +1,34 @@
+require "./spec_helper"
+
+describe Support::Callbacks::CallbackSequence do
+
+  describe "#after #before" do
+    callback_sequence = Callbacks::CallbackSequence.new
+    it "adds proc to array and returns CallbackSequence object" do
+      callback_sequence.before(->{ 1; true }).before(->{ 2; true })
+      callback_sequence.length.should eq 2
+
+      callback_sequence.after(->{ false }).after(->{ false })
+      callback_sequence.length.should eq 4
+    end
+  end
+
+  describe "#call" do
+
+    it "calls callbacks and returns true if all callbacks returns true" do
+      callback_sequence = Callbacks::CallbackSequence.new
+      callback_sequence.before(->{ 1; true }).before(->{ 2; true })
+      callback_sequence.after(->{ 1; true }).after(->{ 2; true })
+      result = callback_sequence.call { "Done" }
+      result.should eq true
+    end
+
+    it "calls callbacks and returns false if at least one callback returns false" do
+      callback_sequence = Callbacks::CallbackSequence.new
+      callback_sequence.before(->{ 1; true }).before(->{ 2; true })
+      callback_sequence.after(->{ 1; false }).after(->{ 2; true })
+      result = callback_sequence.call {}
+      result.should eq false
+    end
+  end
+end
--- src/amethyst/support/callbacks.cr
+++ src/amethyst/support/callbacks.cr
@@ -3,8 +3,8 @@ module Callbacks
 	class CallbackSequence
 
 		def initialize
-			@before = [] of Proc
-			@after  = [] of Proc
+			@before = [] of -> Bool
+			@after  = [] of -> Bool
 		end
 
 		def before(before : Proc)
@@ -17,21 +17,26 @@ module Callbacks
       self
     end
 
+    def length
+    	@before.length+@after.length
+    end
+
     # invokes before callbacks each by one, than run code and invokes after callbacks
 		def call(&block)
 			result = true
-			result = @before.each do |before_callback| 
-				break false unless callback.call
+			@before.each do |before_callback| 
+				break result = false  unless before_callback.call
 			end
 			if result
 				block.call
-        result = @before.each do |before_callback| 
-				  break false unless callback.call
+        @after.each do |after_callback| 
+				  break result = false unless after_callback.call
 				end
 			end
 			result
 		end
 	end
+end
 
 
 			
