93d561c0a7cbcc2915a27f9f69e7442f939f646c
codcore <codcore@gmail.com>
2015-07-07 15:15:35 +0300

    Add status to controller helpers

--- src/amethyst/support/controller_helpers.cr
+++ src/amethyst/support/controller_helpers.cr
@@ -1,16 +1,16 @@
 module ControllerHelpers
-  private def html(body : String)
-    @response.set(200, body)
+  private def html(body : String, status=200)
+    @response.set(status, body)
     @response.header("Content-Type", "text/html")
   end
 
-  private def text(body : String)
-    @response.set(200, body)
+  private def text(body : String, status=200)
+    @response.set(status, body)
     @response.header("Content-Type", "text/plain")
   end
 
-  private def json(body : String)
-    @response.set(200, body)
+  private def json(body : String, status=200)
+    @response.set(status, body)
     @response.header("Content-Type", "application/json")
   end
 
