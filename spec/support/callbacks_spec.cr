de893d8255f300b07ac66028a6a51f6b90007b8d
codcore <codcore@gmail.com>
2015-07-09 18:10:11 +0300

    Add specs for Callbacks

--- spec/support/callbacks_spec.cr
+++ spec/support/callbacks_spec.cr
@@ -1,8 +1,29 @@
 require "./spec_helper"
 
 class CallbacksDefineTest
+  getter test_before
+  getter test_after
+
   include Callbacks
   define_callbacks :first, :second
+  set_callback :first, :before, :before_method
+  set_callback :first, :after, :after_method
+
+  def callback
+    run_callbacks :first do
+      "callback"
+    end
+  end
+
+  def before_method
+    @test_before = 5+8
+    true
+  end
+
+  def after_method
+    @test_after = 6+9
+    true
+  end
 end
 
 describe Support::Callbacks::CallbackSequence do
@@ -17,6 +38,7 @@ describe Support::Callbacks::CallbackSequence do
       callback_sequence.length.should eq 4
     end
   end
+
 
   describe "#call" do
 
@@ -36,12 +58,39 @@ describe Support::Callbacks::CallbackSequence do
       result.should eq false
     end
   end
+end
 
-  describe "define_callbacks" do
-    it "should create class varible contains CallbackSequence" do
-      callback_sequence = CallbacksDefineTest.new
-      callback_sequence._first_callbacks.should be_a Callbacks::CallbackSequence
-      callback_sequence._second_callbacks.should be_a Callbacks::CallbackSequence
-    end
+describe "macro define_callbacks" do
+  it "should create method that returns CallbackSequence" do
+    callback_sequence = CallbacksDefineTest.new
+    callback_sequence._first_callbacks.should be_a Callbacks::CallbackSequence
+    callback_sequence._second_callbacks.should be_a Callbacks::CallbackSequence
+  end
+end
+
+describe "macro set_callbacks" do
+  callback_sequence = CallbacksDefineTest.new
+
+  it "should create instance method that add callback to CallbackSequence" do
+    callback_sequence.responds_to?(:_set_first_before_before_method).should eq true
+  end
+
+  it "created instance method adds callback to CallbackSequence" do
+    callback_sequence._set_first_before_before_method
+    callback_sequence._first_callbacks.length.should eq 1
+  end
+end
+
+describe "macro run_callbacks" do
+  callback_sequence = CallbacksDefineTest.new
+  response = callback_sequence.callback
+
+  it "should run added callbacks" do
+    callback_sequence.test_before.should eq 13
+    callback_sequence.test_after.should eq 15
+  end
+
+  it "should return block value" do
+    response.should eq "callback"
   end
 end
