117f02cd2fe51c29ff17a73c9f727e49c3e1985b
etmnk <antilogic@outlook.com>
2015-10-26 16:32:10 +0900

    fix Nil error of request path

--- src/amethyst/http/request.cr
+++ src/amethyst/http/request.cr
@@ -15,7 +15,7 @@ module Amethyst
 
       def initialize(base_request : HTTP::Request)
         @method  = base_request.method
-        @path    = base_request.path
+        @path    = base_request.path.to_s
         @headers = base_request.headers
         @body    = base_request.body
         @version = base_request.version
